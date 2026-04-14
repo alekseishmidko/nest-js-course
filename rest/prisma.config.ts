118402d97fc68ef6853544db6679117b00754703
alekseishmidko <alexeishmidko@gmail.com>
2026-04-14 23:56:56 +0700

    docker for rest example

--- rest/prisma.config.ts
+++ rest/prisma.config.ts
@@ -3,12 +3,13 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
+console.log(process.env);
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url: process.env["POSTGRES_URI"],
   },
 });
