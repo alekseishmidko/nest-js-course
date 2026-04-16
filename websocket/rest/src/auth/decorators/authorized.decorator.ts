b72f7d1ed1116e2af560ad6d7ea1f2ba5568c26d
alekseishmidko <alexeishmidko@gmail.com>
2026-04-17 00:39:05 +0700

    combine project

---
+++ websocket/rest/src/auth/decorators/authorized.decorator.ts
@@ -0,0 +1,14 @@
+import { createParamDecorator, type ExecutionContext } from '@nestjs/common';
+
+import type { Request } from 'express';
+import { User } from '../../../generated/prisma/browser';
+
+export const Authorized = createParamDecorator(
+  (data: keyof User, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest() as Request;
+    // request.user появляется после успешного прохождения JwtGuard/JwtStrategy.
+    const user = request.user;
+
+    return data ? user![data] : user;
+  },
+);
